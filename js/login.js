534e8ab895fa2a5d2e1be2968020c81e57d169f8
SAURAV KUKADIYA <64151750+sk116@users.noreply.github.com>
2020-08-10 00:34:28 +0530

    .

--- js/login.js
+++ js/login.js
@@ -1,4 +1,8 @@
-function redirectLogin(){
-    var fm = document.getElementsById("myForm");
-    fm.action = "#";
+function redirectLogin(form){
+    var uname = form.username.value;
+    var passwd = form.pass.value;
+    if (uname === "sk116" && passwd === "sk116"){
+        var fm = document.getElementById("loginForm");
+        fm.setAttribute("action","home.html");
+    }
 }
